0ad2e82c4b199d020e639c14e0743a9517c20be9
Esaiasson <teo.esaiasson@gmail.com>
2024-08-12 12:24:14 +0200

    start react server

---
+++ app/client/foodapp/src/creator/CreatorPage.js
@@ -0,0 +1,14 @@
+const getCreator = () =>{
+    
+}
+
+
+const CreatorPage = () => {
+    return (
+        <>
+            <p>Test creator page3</p>
+        </>
+    )
+}
+
+export default CreatorPage
